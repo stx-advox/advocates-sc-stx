c7afb7fcde3025e5eadf43fe2fffa2b1c5444468
Thena Seer <nath.sears@gmail.com>
2022-01-19 13:06:31 -0800

    Create wolf-turtle-bear.js

---
+++ scripts/wolf-turtle-bear.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const stringify = require("json-stable-stringify");
+
+const db = require("better-sqlite3")(
+  "cache/sourcecred/discord/discordMirror-453243919774253079.db"
+);
+
+const aggregate = db
+  .prepare(
+    "select mem.username, r.emoji, count(r.emoji) as count from message_reactions r left join messages m on m.id = r.message_id left join members mem on m.author_id = mem.user_id where mem.bot = 0 and (r.emoji like :wolf or r.emoji like :bear or r.emoji like :turtle) group by mem.user_id, r.emoji"
+  )
+  .all({ wolf: "%🐺%", bear: "%🐻%", turtle: "%🐢%" });
+
+fs.writeFileSync(
+  "output/wtb-aggregate.json",
+  stringify(aggregate, { space: 2 })
+);
+
+const perChannel = db
+  .prepare(
+    "select mem.username, r.emoji, m.channel_id, count(m.channel_id) as count from message_reactions r left join messages m on m.id = r.message_id left join members mem on m.author_id = mem.user_id where mem.bot = 0 and (r.emoji like :wolf or r.emoji like :bear or r.emoji like :turtle) group by mem.user_id, r.emoji, m.channel_id"
+  )
+  .all({ wolf: "%🐺%", bear: "%🐻%", turtle: "%🐢%" });
+
+fs.writeFileSync(
+  "output/wtb-per-channel.json",
+  stringify(perChannel, { space: 2 })
+);
