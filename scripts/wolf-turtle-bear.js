90f9a89bbdfdaebebc99672088c15cd468bfa68b
hz <medo3578@gmail.com>
2022-02-24 21:17:30 +0200

    change to teddy bear

--- scripts/wolf-turtle-bear.js
+++ scripts/wolf-turtle-bear.js
@@ -9,7 +9,7 @@ const aggregate = db
   .prepare(
     "select mem.username, r.emoji, count(r.emoji) as count from message_reactions r left join messages m on m.id = r.message_id left join members mem on m.author_id = mem.user_id where mem.bot = 0 and (r.emoji like :wolf or r.emoji like :bear or r.emoji like :turtle) group by mem.user_id, r.emoji"
   )
-  .all({ wolf: "%🐺%", bear: "%🐻%", turtle: "%🐢%" });
+  .all({ wolf: "%🐺%", bear: "%🧸%", turtle: "%🐢%" });
 
 fs.writeFileSync(
   "output/wtb-aggregate.json",
@@ -20,7 +20,7 @@ const perChannel = db
   .prepare(
     "select mem.username, r.emoji, m.channel_id, count(m.channel_id) as count from message_reactions r left join messages m on m.id = r.message_id left join members mem on m.author_id = mem.user_id where mem.bot = 0 and (r.emoji like :wolf or r.emoji like :bear or r.emoji like :turtle) group by mem.user_id, r.emoji, m.channel_id"
   )
-  .all({ wolf: "%🐺%", bear: "%🐻%", turtle: "%🐢%" });
+  .all({ wolf: "%🐺%", bear: "%🧸%", turtle: "%🐢%" });
 
 fs.writeFileSync(
   "output/wtb-per-channel.json",
